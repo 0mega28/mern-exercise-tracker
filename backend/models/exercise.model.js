df176a29cb062a547f629e0749c173c97b7f8910
Saksham <sakshamdam54321@gmail.com>
2020-11-02 18:16:12 +0530

    Corrected typo

--- backend/models/exercise.model.js
+++ backend/models/exercise.model.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
-const exerciseSchema = new mongoose.model({
+const exerciseSchema = new mongoose.Schema({
     username: {type: String, required: true},
     description: {type: String, required: true},
     duration: {type: Number, required: true},
     date: {type: Date, required: true}
 }, {
-    timestamp: true
+    timestamps: true
 });
 
 module.exports = mongoose.model('Exercise', exerciseSchema);
