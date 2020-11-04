9fe53993881a4e01029019c9e8e8d63bd69d2823
Saksham <sakshamdam54321@gmail.com>
2020-11-04 11:09:24 +0530

    Corrected typo

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -3,7 +3,7 @@ let Exercise = require('../models/exercise.model');
 
 router.route('/').get((req, res) => {
     Exercise.find()
-    .then(exercies => res.json(exercies))
+    .then(exercise => res.json(exercise))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
